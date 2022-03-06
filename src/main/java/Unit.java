8057b4d1b8b26e054630d415213ec60f05485a59
J <jostein.aune@gmail.com>
2022-03-06 17:42:15 +0100

    Fixed attack method and added abstract bonus methods.

--- src/main/java/Unit.java
+++ src/main/java/Unit.java
@@ -1,8 +1,9 @@
-public class Unit {
+public abstract class Unit {
     String name;
     int health;
     int attack;
     int armor;
+
 
     public Unit(String name, int health, int attack, int armor) {
         this.name = name;
@@ -11,29 +12,48 @@ public class Unit {
         this.armor = armor;
     }
 
-    public void attack(Unit opponent) {
-        System.out.println(opponent + " attacking ");
+    public void attack(Unit opponent){
+        if(attack + this.getAttackBonus()>= this.health + this.getResistBonus()){ // 0 hp
+            System.out.println(this.name + " died"); // boolean isDead()?
+        } else{
+            this.health = (this.health + this.getResistBonus()) - (attack + this.getAttackBonus());
+            System.out.println("Remaining life for " + this.name + " equals " + this.health);
+        }
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getHealth() {
         return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
     }
 
     public int getAttack() {
         return attack;
     }
 
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
     public int getArmor() {
         return armor;
     }
 
-    public void setHealth(int health) {
-        this.health = health;
+    public void setArmor(int armor) {
+        this.armor = armor;
     }
+    public abstract int getAttackBonus();
+    public abstract int getResistBonus();
 
     public String toString() {
         return "Unit\n" + "Name: " + name + "\n" + "Hitpoints: " + health + "\n" + "Attack: " + attack + "\n" + "Armor: " + armor;
