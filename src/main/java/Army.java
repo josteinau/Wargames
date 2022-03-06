e9a3dc1c5d63eedb665b7a008ef81c41012f9853
J <jostein.aune@gmail.com>
2022-03-06 23:40:49 +0100

    Added army class, implemented some of the required methods. Did not test yet, assigment deadline in 20 minutes.

---
+++ src/main/java/Army.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Army {
+    String name;
+    List<Unit> units;
+    public Army(String name){
+        this.name = name;
+    }
+    public Army(String name, List<Unit> units){
+        this.name = name;
+        this.units = new ArrayList<Unit>();
+    }
+    public String getName(){
+        return name;
+    }
+    public void add(Unit newUnit){
+        units.add(newUnit);
+    }
+    public void addAll(List<Unit> units){
+
+    }
+    public void remove(Unit unit){
+        units.remove(unit);
+    }
+    public boolean hasUnits(){
+        if(units.isEmpty() || units == null){
+            return false;
+        }
+        return true;
+    }
+    public void getAllUnits(List<Unit> units){
+        for(int i = 0; i< units.size(); i++){
+            System.out.println(units.get(i) + " ");
+        }
+    }
+    public Unit getRandomElement(){
+        Random rand = new Random();
+        int index = rand.nextInt(units.size());
+        Unit randomUnit = units.get(index);
+        return randomUnit;
+    }
+    public String toString(){ // Add more toString
+        return "Army with name: " + name + "";
+    }
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        else if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Army otherArmy = (Army) obj;
+        return this.name.equals(otherArmy.name);
+    }
+}
