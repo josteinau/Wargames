5423845b8c4ceab6206a7cb65bf3a13073270620
J <jostein.aune@gmail.com>
2022-03-06 19:59:11 +0100

    Added CommanderUnit Class with 2 constructors

---
+++ src/main/java/CommanderUnit.java
@@ -0,0 +1,10 @@
+public class CommanderUnit extends CavalryUnit{
+
+    public CommanderUnit(String name, int health, int attack, int armor) {
+        super(name, health, attack, armor);
+    }
+
+    public CommanderUnit(String name, int health) {
+        super(name, health, 25,15);
+    }
+}
